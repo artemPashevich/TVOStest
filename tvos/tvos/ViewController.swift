d5b20d433603b3e9d739f1412478295817d03fe2
artemPashevich <100874372+artemPashevich@users.noreply.github.com>
2023-02-12 02:09:52 +0300

    start

---
+++ tvos/tvos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tvos
+//
+//  Created by Артем Пашевич on 12.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
